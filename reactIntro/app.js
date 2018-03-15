4936a1c991791d206ea7122b11f150cfd250e8d3
mikiskela <mikiskela1992@gmail.com>
2018-03-15 23:45:12 +0100

    Neuspela modifikacija

--- reactIntro/app.js
+++ reactIntro/app.js
@@ -1,14 +1,49 @@
-const Products = () => {
-    return <li>My product</li>
+const productList = ["Apple", "Bannana", "Orange"]
+
+
+// const Myproducts = () => {
+//     const myArrayProducts = [];
+//     productList.forEach((product, index) =>{  
+       
+//     myArrayProducts.push(<Products name={name} key={index}/>)
+//     })
+//     return myArrayProducts
+// }
+// console.log(Myproducts());
+
+const Myproducts = () => {
+    return productList.map((product, index) => {
+        const {name} = product.toUpperCase();
+        console.log(name);
+        
+        return <Products />
+    })
 }
+
+
+
+
+
+const Products = (props) => {
+    const {name, index} = props
+    console.log(props);
+    
+    return <li key={index}>{name}</li>
+}
+
+
+
+
 
 const ShoppingList = () => {
     return (
         <ul>
-            <Products />
+            {Myproducts()} 
         </ul>
     )
 }
+
+
 
 const App = () => {
     return (
@@ -18,5 +53,7 @@ const App = () => {
         </div>
     )
 }
+
+
 const rootElement = document.querySelector(".root");
 ReactDOM.render(<App />, rootElement);
